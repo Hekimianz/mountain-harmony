6320eb8868395937d327ec890f4a458dc5a7ebbc
Hekimianz <aramhekdesign@gmail.com>
2024-01-10 11:12:44 -0600

    Added register endpoint

---
+++ controllers/Users.js
@@ -0,0 +1,52 @@
+const pool = require("../models/db");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+module.exports = {
+  register: [
+    // Validation middleware
+    body("username")
+      .isLength({ min: 5 })
+      .withMessage("Username must be at least 5 characters long"),
+    body("email").isEmail().withMessage("Invalid email address"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+
+    // Controller method
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      try {
+        const { username, password, name, email } = req.body;
+
+        // Check if user already exists
+        const userCheck = await pool.query(
+          "SELECT * FROM users WHERE email = $1",
+          [email]
+        );
+        if (userCheck.rows.length > 0) {
+          return res.status(409).json({ message: "Username already exists" });
+        }
+
+        // Hash password
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        // Store new user in the database
+        const newUser = await pool.query(
+          "INSERT INTO users (name, email, username, password) VALUES ($1, $2, $3, $4) RETURNING *",
+          [name, email, username, hashedPassword]
+        );
+
+        // Exclude password from the response
+        const { password: _, ...userData } = newUser.rows[0];
+        res.status(201).json(userData);
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
+    },
+  ],
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 const pool = require("./models/db");
 const productsRoutes = require("./routes/Products");
+const userRoutes = require("./routes/Users");
 
 const PORT = process.env.SERVER_PORT || 4000;
 
@@ -12,8 +13,10 @@ app.use(
     origin: "http://localhost:3000", // Replace with the URL of your React app
   })
 );
+app.use(express.json());
 
 app.use("/products", productsRoutes);
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on localhost:${PORT}`);
